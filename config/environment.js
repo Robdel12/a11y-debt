f655cdc73a6fc5454a5a95ba2c12db900dd54096
Robdel12 <robertdeluca19@gmail.com>
2015-10-30 00:20:28 -0500

    Add gh-pages addon

--- config/environment.js
+++ config/environment.js
@@ -4,8 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'a11y-debt',
     environment: environment,
-    baseURL: '/',
-    locationType: 'auto',
+    baseURL: '/a11y-debt',
+    locationType: 'hash',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -40,7 +40,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.baseURL = '/a11y-debt';
   }
 
   return ENV;
