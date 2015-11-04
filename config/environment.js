7cc3dc9df0647856b322de289f7066090ef68443
Robdel12 <robertdeluca19@gmail.com>
2015-11-03 22:36:49 -0600

    Try rooturl for assets

--- config/environment.js
+++ config/environment.js
@@ -40,6 +40,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     ENV.baseURL = '/a11y-debt';
+    ENV.rootURL = '/a11y-debt';
     ENV.locationType = 'hash';
   }
 
