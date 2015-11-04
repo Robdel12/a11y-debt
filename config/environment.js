dc413f5bee0ef195c265ef6d71b1b2cb9c7eda0a
Robdel12 <robertdeluca19@gmail.com>
2015-11-03 22:42:35 -0600

    Remove baseURL config

--- config/environment.js
+++ config/environment.js
@@ -39,8 +39,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.baseURL = '/a11y-debt';
-    ENV.rootURL = '/a11y-debt';
+    ENV.rootURL = '/a11y-debt/';
     ENV.locationType = 'hash';
   }
 
