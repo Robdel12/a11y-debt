b36b50e94056dd6a96bf356044193c343d8175f0
Robdel12 <robertdeluca19@gmail.com>
2015-11-06 11:22:13 -0600

    Add iOS style swiping to navigate

--- app/routes/slide.js
+++ app/routes/slide.js
@@ -4,6 +4,9 @@ import presentation from '../presentation';
 let $ = Ember.$;
 
 export default Ember.Route.extend({
+  xDown: null,
+  yDown: null,
+
   pageTracker: Ember.inject.service('page-tracker'),
   model(params) {
     var index = presentation.indexOf(params.id);
@@ -29,7 +32,10 @@ export default Ember.Route.extend({
   },
 
   activate() {
-    $(window).on('keydown.SlideRoute', (e)=> {
+    document.addEventListener('touchstart', Ember.run.bind(this, this.handleTouchStart), false);
+    document.addEventListener('touchmove', Ember.run.bind(this, this.handleTouchMove), false);
+
+    $(window).on('keydown.SlideRoute', (e) => {
       switch (e.keyCode) {
       case 37:
         this.move(-1);
@@ -43,5 +49,36 @@ export default Ember.Route.extend({
 
   deactivate() {
     $(window).off('keydown.SlideRoute');
+  },
+
+  handleTouchStart(evt) {
+    Ember.set(this, 'xDown', evt.touches[0].clientX);
+    Ember.set(this, 'yDown', evt.touches[0].clientY);
+  },
+
+  handleTouchMove(evt) {
+    let xDown = Ember.get(this, 'xDown');
+    let yDown = Ember.get(this, 'yDown');
+
+    if ( ! xDown || ! yDown ) {
+      return;
+    }
+
+    var xUp = evt.touches[0].clientX;
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+      if ( xDiff > 0 ) {
+        this.move(1);
+      } else {
+        this.move(-1);
+      }
+    }
+    /* reset values */
+    Ember.set(this, 'xDown', null);
+    Ember.set(this, 'yDown', null);
   }
 });
